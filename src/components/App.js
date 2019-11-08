a7abfd575c2729aeea8e631fb7ecd2df823e3c09
Lion-Star <15007006754@163.com>
2019-11-08 21:03:51 +0800

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+    constructor(){
+        super();
+        this.state = {
+
+        };
+    }
+    handleClick = ()=>{
+        
+    }
+    render(){
+        return (
+            <div className="app">
+				
+            </div>
+        )
+    }
+}
+
+export default App;
+
